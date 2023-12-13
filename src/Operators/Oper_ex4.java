eca4a80d4b364e5ed9742667c1fbd23c43541422
Ishimura94 <60233347+Ishimura94@users.noreply.github.com>
2023-12-13 22:28:12 +0300

    Update Oper_ex4.java

--- src/Operators/Oper_ex4.java
+++ src/Operators/Oper_ex4.java
@@ -19,10 +19,10 @@ public class Oper_ex4 {
         System.out.println(mid);
 
         double halfmid = mid/2;
-        System.out.println(halfmid);
+        //System.out.println(halfmid); //Вывод значения для проверки корректности работы, по условию не нужен
 
         double result = Math.floor(halfmid);
-        System.out.println(result);
+        //System.out.println(result); //Вывод значения для проверки корректности работы, по условию не нужен
 
         String progEnd = (result > 3) ? "Программа выполнена корректно" : "На этот случай текста не было, но пусть будет, что программа выполнена некорректно" ;
         System.out.println(progEnd);
